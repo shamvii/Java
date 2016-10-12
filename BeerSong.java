d87426f2eb4fca000ffd18a85a3196fd14216a20
shamvii <shambhavishukla24@gmail.com>
2016-10-12 16:41:28 -0400

    BeerSong Code

---
+++ BeerSong.java
@@ -0,0 +1,22 @@
+public class BeerSong {
+public static void main(String[] args) {
+int beernum = 99;
+String word = "bottles";
+while (beernum > 0){
+if(beernum == 1) {
+word = "bottle";
+}
+System.out.println(beernum + " " + word + " of beer on the wall");
+System.out.println(beernum + " " + word + " of beer.");
+System.out.println("Take one down.");
+System.out.println("Pass it around.");
+beernum = beernum - 1;
+if (beernum > 0){
+System.out.println(beernum + " " + word + " of beer on the wall");
+}
+else {
+System.out.println("no more bottles of beer on the wall");
+}
+}
+}
+}
